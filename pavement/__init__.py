95f7b63cc8cf4ef7075de261ddfc812af0b02bfb
Aaron Schumacher <ajschumacher@gmail.com>
2026-05-12 20:43:44 -0400

    plotting code

--- pavement/__init__.py
+++ pavement/__init__.py
@@ -1,3 +1,8 @@
+from collections import Counter
+
+import matplotlib.pyplot as plt
+
+
 def quantiles(data, levels, weights=None):
     """Type 2 quantiles for sorted data, possibly weighted"""
     # https://robjhyndman.com/papers/sample_quantiles.pdf
@@ -29,3 +34,37 @@ assert quantiles([1, 2], [0.5]) == [1.5]
 assert quantiles([1, 2], [0.5, 0.8], [4, 1]) == [1, 1.5]
 assert quantiles([1, 2, 3, 4, 5], [1]) == [5]
 assert quantiles([1, 2, 3, 4, 5], [0.5, 1]) == [3, 5]
+
+
+def plot(data, weights=None,
+         bins=4, ypos=0, height=0.6,
+         whisker=0.1, show_whiskers=False):
+    levels = list(x/bins for x in range(bins + 1))
+    # TODO: sort weights properly?
+    values = quantiles(sorted(data), levels, weights)
+    plt.vlines(values,
+               ymin=ypos - height/2, ymax=ypos + height/2,
+               color='black')
+    dupes = [x for x, n in Counter(values).items() if n > 1]
+    if dupes and show_whiskers:
+        plt.vlines(dupes,
+                   ymin=ypos - height/2 - whisker,
+                   ymax=ypos + height/2 + whisker,
+                   color='black')
+    plt.hlines([ypos - height/2, ypos + height/2],
+               xmin=values[0], xmax=values[-1],
+               color='black')
+
+def multi(data, categories, labels=None, weights=None,
+          bins=4, height=0.6,
+          whisker=0.1, show_whiskers=False):
+    if labels is None:
+        labels = sorted(list(set(categories)))
+    for index, label in enumerate(labels[::-1]):
+        subdata = [d for d, c in zip(data, categories) if c == label]
+        # TODO: sort weights properly?
+        subweights = ([w for w, c in zip(weights, categories) if c == label]
+                      if weights is not None else None)
+        plot(subdata, weights=subweights, bins=bins, ypos=index, height=height,
+             whisker=whisker, show_whiskers=show_whiskers)
+    plt.gca().set_yticks(list(range(len(labels))), labels[::-1])
